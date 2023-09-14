f8f69fa1e0a3d8d2646ab3bdc93be6ce1f133d6a
dolsen0 <derek.olsen@gmail.com>
2023-09-14 16:59:00 -0400

    adjusted error handling for getWeather

--- controller/getAstro.js
+++ controller/getAstro.js
@@ -3,7 +3,7 @@ import axios from "axios"
 let date = "2023-09-14";
 
 export default function getAstro(req, res){
-    let location = req.params.location
+    let location = req.params.location;
     axios.get(`http://api.weatherapi.com/v1/astronomy.json?key=${process.env.KEY}&q=${location}&dt=${date}
     `)
     .then((response) => {
--- controller/getWeather.js
+++ controller/getWeather.js
@@ -10,8 +10,15 @@ export default function getWeather(req, res) {
     .then(data => {
         res.send(data);
     })
-    .catch(error => {
-        console.log(error);
-        res.status(500).send('Failed to fetch weather data.');
-    });
+    .catch((error) => {
+        if (error.response) {
+          console.log('Error status:', error.response.status);
+          console.log('Error data:', error.response.data);
+        } else if (error.request) {
+          console.log('No response was received', error.request);
+        } else {
+          console.log('Error', error.message);
+        }
+        res.status(500).send("Failed to fetch weather data.");
+      });
 }
